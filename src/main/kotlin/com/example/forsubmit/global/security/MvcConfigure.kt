e61cdadc2e8697ec1ea0f262c66093b0005b8b52
jhhong0509 <jhhong0509@naver.com>
2021-12-03 19:42:20 +0900

    ⚡️ :: (#6) MvcConfigure 추가

---
+++ src/main/kotlin/com/example/forsubmit/global/security/MvcConfigure.kt
@@ -0,0 +1,14 @@
+package com.example.forsubmit.global.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class MvcConfigure : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+    }
+}
