83d43d4a2822d048f6fbc248893d3e6f65739838
jhhong0509 <jhhong0509@naver.com>
2021-12-05 20:26:29 +0900

    ⚡️ :: (#19) 채팅 엔티티 추가

---
+++ src/main/kotlin/com/example/forsubmit/domain/chat/entity/chat/Chat.kt
@@ -0,0 +1,34 @@
+package com.example.forsubmit.domain.chat.entity.chat
+
+import com.example.forsubmit.domain.chat.entity.chatreader.ChatReader
+import com.example.forsubmit.domain.chatroom.entity.ChatRoom
+import com.example.forsubmit.domain.user.entity.User
+import org.jetbrains.annotations.NotNull
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@EntityListeners(AuditingEntityListener::class)
+class Chat(
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @NotNull
+    val message: String,
+
+    @CreatedDate
+    @NotNull
+    val createdAt: LocalDateTime,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatroom_id")
+    val chatRoom: ChatRoom,
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    val user: User,
+
+)
