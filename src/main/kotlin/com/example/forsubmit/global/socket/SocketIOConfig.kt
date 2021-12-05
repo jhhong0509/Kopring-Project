bc058ec6b6b6fe996bceefba8db3d833a8948458
jhhong0509 <jhhong0509@naver.com>
2021-12-06 00:34:10 +0900

    :recycle: :: (#66) SocketIO 라이브러리로 변경

--- src/main/kotlin/com/example/forsubmit/global/socket/SocketIOConfig.kt
+++ src/main/kotlin/com/example/forsubmit/global/socket/SocketIOConfig.kt
@@ -2,31 +2,31 @@ package com.example.forsubmit.global.socket
 
 import com.corundumstudio.socketio.Configuration
 import com.corundumstudio.socketio.SocketIOServer
+import com.corundumstudio.socketio.annotation.SpringAnnotationScanner
 import com.example.forsubmit.global.socket.error.SocketErrorController
 import com.example.forsubmit.global.socket.property.SocketProperties
-import com.example.forsubmit.global.socket.security.SocketConnectController
 import org.springframework.context.annotation.Bean
+import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 
+
+@Order(Int.MAX_VALUE)
 @Component
 class SocketIOConfig(
-    private val socketIOSupporter: SocketIOAddHandlerSupporter,
-    private val socketProperties: SocketProperties,
     private val socketErrorController: SocketErrorController,
-    private val socketConnectController: SocketConnectController
+    private val socketProperties: SocketProperties
 ) {
     @Bean
     fun socketIOServer(): SocketIOServer {
         val config = Configuration()
         config.port = socketProperties.port
         config.origin = "*"
-        val socketIOServer = SocketIOServer(config)
+        config.exceptionListener = socketErrorController
+        return SocketIOServer(config)
+    }
 
-        socketIOSupporter.addListeners(socketIOServer)
-
-        socketIOServer.addListeners(socketErrorController)
-        socketIOServer.addConnectListener(socketConnectController::onConnect)
-
-        return socketIOServer
+    @Bean
+    fun springAnnotationScanner(socketServer: SocketIOServer): SpringAnnotationScanner {
+        return SpringAnnotationScanner(socketServer)
     }
 }
