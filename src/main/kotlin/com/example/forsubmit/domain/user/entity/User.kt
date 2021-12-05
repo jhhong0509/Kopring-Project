e6a0d33764352b728c4451693ab3df1286541a62
jhhong0509 <jhhong0509@naver.com>
2021-12-06 08:04:33 +0900

    ⚡️ :: (#63) User에 관계매핑 추가

--- src/main/kotlin/com/example/forsubmit/domain/user/entity/User.kt
+++ src/main/kotlin/com/example/forsubmit/domain/user/entity/User.kt
@@ -1,5 +1,8 @@
 package com.example.forsubmit.domain.user.entity
 
+import com.example.forsubmit.domain.chat.entity.chat.Chat
+import com.example.forsubmit.domain.chat.entity.chatreader.ChatReader
+import com.example.forsubmit.domain.chatroom.entity.member.ChatRoomMember
 import org.hibernate.annotations.NaturalId
 import javax.persistence.*
 
@@ -16,5 +19,14 @@ data class User(
     val name: String,
 
     @Column(nullable = false)
-    val password: String
+    val password: String,
+
+    @OneToMany(mappedBy = "user")
+    val chat: MutableList<Chat> = mutableListOf(),
+
+    @OneToMany(mappedBy = "chatReaderId.user")
+    val chatReader: MutableList<ChatReader> = mutableListOf(),
+
+    @OneToMany(mappedBy = "chatRoomMemberId.user")
+    val chatRoomMember: MutableList<ChatRoomMember> = mutableListOf()
 )
