b8cdc8dcc0e5d80dbaa36d4e020e9c2a0dd00195
jhhong0509 <jhhong0509@naver.com>
2021-12-06 11:32:00 +0900

    :test_tube: :: (#14) Post 테스트 추가

--- src/test/groovy/com/example/forsubmit/domain/post/entity/manager/PostRepositoryManagerTest.groovy
+++ src/test/groovy/com/example/forsubmit/domain/post/entity/manager/PostRepositoryManagerTest.groovy
@@ -4,6 +4,7 @@ import com.example.forsubmit.domain.post.entity.Post
 import com.example.forsubmit.domain.post.entity.PostRepository
 import com.example.forsubmit.domain.post.exceptions.PostNotFoundException
 import com.example.forsubmit.domain.user.entity.User
+import org.springframework.data.domain.PageRequest
 import spock.lang.Specification
 
 class PostRepositoryManagerTest extends Specification {
@@ -60,4 +61,47 @@ class PostRepositoryManagerTest extends Specification {
         1  | _
         2  | _
     }
+
+    def "Delete Post Success"() {
+        given:
+        def user = new User()
+        def post = new Post(id, "title", "content", null, user)
+
+        when:
+        postRepositoryManager.delete(post)
+
+        then:
+        postRepository.delete(post)
+        noExceptionThrown()
+
+        where:
+        id | _
+        1  | _
+        2  | _
+    }
+
+    def "Get Post Page Success"() {
+        given:
+        def pageRequest = PageRequest.of(page, size)
+        def user = new User()
+        def post1 = new Post(1, "title", "content", null, user)
+        def post2 = new Post(2, "title", "content", null, user)
+        def post3 = new Post(3, "title", "content", null, user)
+        def post4 = new Post(4, "title", "content", null, user)
+
+        def postList = List.of(post1, post2, post3, post4)
+
+        when:
+        def pageResponse = postRepositoryManager.findAllPagination(pageRequest)
+
+        then:
+        postRepository.findAll(pageRequest) >> postList
+        noExceptionThrown()
+
+        where:
+        page | size
+        0    | 2
+        2    | 1
+    }
+
 }
