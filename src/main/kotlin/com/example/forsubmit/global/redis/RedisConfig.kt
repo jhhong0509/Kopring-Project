9aa92c7958734d5a32a38c778b7b7c12211f8093
jhhong0509 <jhhong0509@naver.com>
2021-12-06 10:11:36 +0900

    :bug: :: (#70) EmbeddedRedis 수정

--- src/main/kotlin/com/example/forsubmit/global/redis/RedisConfig.kt
+++ src/main/kotlin/com/example/forsubmit/global/redis/RedisConfig.kt
@@ -1,5 +1,6 @@
 package com.example.forsubmit.global.redis
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
@@ -10,6 +11,7 @@ class RedisConfig(
     private val redisProperty: RedisProperty
 ) {
     @Bean
+    @ConditionalOnMissingBean(RedisConnectionFactory::class)
     fun redisConnectionFactory(): RedisConnectionFactory {
         return LettuceConnectionFactory(redisProperty.host, redisProperty.port)
     }
