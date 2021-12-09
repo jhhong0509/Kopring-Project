b2ee7d5822a6d7d7686fb5bdd97f5045fe3d9110
jhhong0509 <jhhong0509@naver.com>
2021-12-09 15:01:27 +0900

    ⚡️ :: (#75) 테스트일때 SockdetIO실행 안되게 변경

--- src/main/kotlin/com/example/forsubmit/global/socket/SocketRunner.kt
+++ src/main/kotlin/com/example/forsubmit/global/socket/SocketRunner.kt
@@ -2,9 +2,11 @@ package com.example.forsubmit.global.socket
 
 import com.corundumstudio.socketio.SocketIOServer
 import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
 @Component
+@Profile("!test")
 class SocketRunner(
     private val socketIOServer: SocketIOServer
 ) : CommandLineRunner {
