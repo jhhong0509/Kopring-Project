ea4ee91053d24a81b91a9cdc6955194bbe2a87a3
jhhong0509 <jhhong0509@naver.com>
2021-12-13 07:51:05 +0900

    ⚡️ :: (#88) PropertyScan을 Config로 뺌

--- src/main/kotlin/com/example/forsubmit/ForSubmitApplication.kt
+++ src/main/kotlin/com/example/forsubmit/ForSubmitApplication.kt
@@ -1,11 +1,8 @@
 package com.example.forsubmit
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
 class ForSubmitApplication
 
---
+++ src/main/kotlin/com/example/forsubmit/PropertyScanConfig.kt
@@ -0,0 +1,9 @@
+package com.example.forsubmit
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationPropertiesScan
+class PropertyScanConfig {
+}
