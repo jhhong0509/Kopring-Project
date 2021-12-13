56141f2f52922c5b28485db00fe88fed51506acb
jhhong0509 <jhhong0509@naver.com>
2021-12-14 08:44:52 +0900

    ⚡️ :: (#82) QueryDSL 설정 추가

--- src/main/kotlin/com/example/forsubmit/global/querydsl/QueryDSLConfig.kt
+++ src/main/kotlin/com/example/forsubmit/global/querydsl/QueryDSLConfig.kt
@@ -1,4 +1,16 @@
 package com.example.forsubmit.global.querydsl
 
-class QueryDSLConfig {
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+
+@Configuration
+class QueryDSLConfig(
+    private val entityManager: EntityManager
+) {
+
+    @Bean
+    fun jpaQueryFactory() = JPAQueryFactory(entityManager)
+
 }
