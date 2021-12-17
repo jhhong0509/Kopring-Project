03d7b3cfea653176e20b2f5b690df0640bc719c4
jhhong0509 <jhhong0509@naver.com>
2021-12-18 00:05:43 +0900

    ⚡️ :: (#95) PostService 추가

---
+++ src/main/kotlin/com/example/forsubmit/domain/post/service/PostService.kt
@@ -0,0 +1,100 @@
+package com.example.forsubmit.domain.post.service
+
+import com.example.forsubmit.domain.post.entity.Post
+import com.example.forsubmit.domain.post.entity.PostRepository
+import com.example.forsubmit.domain.post.exceptions.CannotUpdatePostException
+import com.example.forsubmit.domain.post.exceptions.PostNotFoundException
+import com.example.forsubmit.domain.post.payload.request.CreatePostRequest
+import com.example.forsubmit.domain.post.payload.request.UpdatePostRequest
+import com.example.forsubmit.domain.post.payload.response.PostContentResponse
+import com.example.forsubmit.domain.post.payload.response.PostListResponse
+import com.example.forsubmit.domain.post.payload.response.PostResponse
+import com.example.forsubmit.domain.user.facade.UserFacade
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
+
+@Service
+class PostService(
+    private val postRepository: PostRepository,
+    private val userFacade: UserFacade
+) {
+    fun savePost(request: CreatePostRequest) {
+        val user = userFacade.findCurrentUser()
+
+        val post = Post(
+            user = user,
+            title = request.title,
+            content = request.content
+        )
+
+        postRepository.save(post)
+    }
+
+    @Transactional
+    fun updatePost(id: Long, request: UpdatePostRequest) {
+        val user = userFacade.findCurrentUser()
+        val post = findPostById(id)
+
+        if (post.user != user) {
+            throw CannotUpdatePostException.EXCEPTION
+        }
+
+        post.update(
+            title = request.title,
+            content = request.content
+        )
+    }
+
+    fun deletePost(id: Long) {
+        val user = userFacade.findCurrentUser()
+        val post = findPostById(id)
+
+        if (post.user != user) {
+            throw CannotUpdatePostException.EXCEPTION
+        }
+
+        postRepository.delete(post)
+    }
+
+    fun getSinglePost(id: Long): PostContentResponse {
+        val post = findPostById(id)
+        val user = userFacade.findCurrentUser()
+
+        return PostContentResponse(
+            content = post.content,
+            title = post.title,
+            createdAt = post.createdDate ?: LocalDateTime.now(),
+            userEmail = user.email,
+            userName = user.name
+        )
+    }
+
+    fun getPostList(pageable: Pageable): PostListResponse {
+        val postPage = postRepository.findAllBy(pageable)
+
+        val postList = postPage.content
+            .map {
+                PostResponse(
+                    userName = it.user.name,
+                    userEmail = it.user.email,
+                    createdAt = it.createdDate,
+                    title = it.title
+                )
+            }
+            .toCollection(mutableListOf())
+
+        return PostListResponse(
+            responses = postList,
+            hasNextPage = postPage.hasNext()
+        )
+
+    }
+
+    private fun findPostById(id: Long): Post {
+        return postRepository.findByIdOrNull(id) ?: throw PostNotFoundException.EXCEPTION
+    }
+
+}
