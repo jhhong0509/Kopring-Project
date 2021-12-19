2ff9b9012a05ad81e68ac65b642800d80edc1d2e
jhhong0509 <jhhong0509@naver.com>
2021-12-20 00:50:30 +0900

    ⚡️ :: (#95) 실수 수정

--- src/main/kotlin/com/example/forsubmit/PropertyScanConfig.kt
+++ src/main/kotlin/com/example/forsubmit/PropertyScanConfig.kt
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-
+@ConfigurationPropertiesScan
 class PropertyScanConfig {
 }
