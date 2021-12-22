02789a1ad0dfffd5413629a8651ae9de129bb77d
jhhong0509 <jhhong0509@naver.com>
2021-12-22 14:39:54 +0900

    :bug: :: (#108) payload 형식 변경

--- src/main/kotlin/com/example/forsubmit/domain/user/payload/request/SignUpRequest.kt
+++ src/main/kotlin/com/example/forsubmit/domain/user/payload/request/SignUpRequest.kt
@@ -1,15 +1,19 @@
 package com.example.forsubmit.domain.user.payload.request
 
+import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
-class SignUpRequest(
-    @field:NotBlank
-    val email: String,
+class SignUpRequest {
+    @field:Email
+    lateinit var email: String
+        private set
 
     @field:NotBlank
-    val name: String,
+    lateinit var name: String
+        private set
 
     @field:Size(min = 6, max = 20)
-    val password: String,
-)
+    lateinit var password: String
+        private set
+}
