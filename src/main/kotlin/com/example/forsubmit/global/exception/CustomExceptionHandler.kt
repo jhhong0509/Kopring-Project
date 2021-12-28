4d47b6199549c74be97eba441f024b9841b0f94a
jhhong0509 <jhhong0509@naver.com>
2021-12-28 19:35:34 +0900

    ⚡️ :: (#117) 예외 발생 시 로깅 로직 추가

--- src/main/kotlin/com/example/forsubmit/global/exception/CustomExceptionHandler.kt
+++ src/main/kotlin/com/example/forsubmit/global/exception/CustomExceptionHandler.kt
@@ -3,12 +3,16 @@ package com.example.forsubmit.global.exception
 import com.example.forsubmit.global.exception.exceptions.InvalidMethodArgumentException
 import com.example.forsubmit.global.exception.exceptions.RequestNotFoundException
 import com.example.forsubmit.global.payload.BaseResponse
+import mu.KLogger
+import mu.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.servlet.NoHandlerFoundException
+
+private val kLogger: KLogger = KotlinLogging.logger {}
 
 @ControllerAdvice
 class CustomExceptionHandler {
@@ -29,6 +33,7 @@ class CustomExceptionHandler {
     }
 
     private fun handleException(e: GlobalException): ResponseEntity<BaseResponse<Unit>> {
+        kLogger.error { "Error Occurred: ${e.errorMessage}" }
         val httpStatus = HttpStatus.valueOf(e.status)
         val body = BaseResponse.of(e)
         return ResponseEntity(body, httpStatus)
--- src/main/kotlin/com/example/forsubmit/global/exception/ExceptionFilter.kt
+++ src/main/kotlin/com/example/forsubmit/global/exception/ExceptionFilter.kt
@@ -1,20 +1,21 @@
 package com.example.forsubmit.global.exception
 
 import com.example.forsubmit.global.exception.exceptions.InternalServerError
-import com.example.forsubmit.global.exception.exceptions.InvalidMethodArgumentException
-import com.example.forsubmit.global.exception.exceptions.RequestNotFoundException
 import com.example.forsubmit.global.payload.BaseResponse
+import com.fasterxml.jackson.databind.ObjectMapper
+import mu.KLogger
+import mu.KotlinLogging
 import org.springframework.http.MediaType
-import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.filter.OncePerRequestFilter
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
-import org.springframework.web.servlet.NoHandlerFoundException
-import org.springframework.web.util.NestedServletException
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-class ExceptionFilter : OncePerRequestFilter() {
+private val kLogger: KLogger = KotlinLogging.logger {}
+
+class ExceptionFilter(
+    private val objectMapper: ObjectMapper,
+) : OncePerRequestFilter() {
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
@@ -23,7 +24,8 @@ class ExceptionFilter : OncePerRequestFilter() {
         try {
             filterChain.doFilter(request, response)
         } catch (exception: Exception) {
-            when(exception) {
+            kLogger.error(exception.message)
+            when (exception) {
                 is GlobalException -> writeErrorCode(exception, response)
                 else -> writeErrorCode(InternalServerError.EXCEPTION, response)
             }
