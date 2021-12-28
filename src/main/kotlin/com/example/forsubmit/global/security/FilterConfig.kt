253d9c8466f236b066f7edf036b7e5daf2cc80f5
jhhong0509 <jhhong0509@naver.com>
2021-12-29 00:13:36 +0900

    ⚡️ :: (#117) FilterConfig에 Log추가

--- src/main/kotlin/com/example/forsubmit/global/security/FilterConfig.kt
+++ src/main/kotlin/com/example/forsubmit/global/security/FilterConfig.kt
@@ -1,19 +1,24 @@
 package com.example.forsubmit.global.security
 
 import com.example.forsubmit.global.exception.ExceptionFilter
+import com.example.forsubmit.global.logger.LogFilter
 import com.example.forsubmit.global.security.jwt.JwtTokenProvider
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 class FilterConfig(
-    private val jwtTokenProvider: JwtTokenProvider
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val objectMapper: ObjectMapper
 ) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
     override fun configure(builder: HttpSecurity) {
         val tokenFilter = TokenFilter(jwtTokenProvider)
-        val exceptionFilter = ExceptionFilter()
+        val exceptionFilter = ExceptionFilter(objectMapper)
+        val logFilter = LogFilter()
         builder.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter::class.java)
         builder.addFilterBefore(exceptionFilter, TokenFilter::class.java)
+        builder.addFilterBefore(logFilter, ExceptionFilter::class.java)
     }
 }
