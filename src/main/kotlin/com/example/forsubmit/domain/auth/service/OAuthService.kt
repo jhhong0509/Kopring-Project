7e4d5f35115db040b60c2f420eea65947a53d153
jhhong0509 <jhhong0509@naver.com>
2022-01-09 00:42:37 +0900

    ⚡️ :: (#125) OAuthService 수정

--- src/main/kotlin/com/example/forsubmit/domain/auth/service/OAuthService.kt
+++ src/main/kotlin/com/example/forsubmit/domain/auth/service/OAuthService.kt
@@ -1,20 +1,21 @@
 package com.example.forsubmit.domain.auth.service
 
 import com.example.forsubmit.domain.auth.exceptions.InvalidOauthTypeException
-import com.example.forsubmit.domain.auth.payload.response.OAuthRedirectUrlResponse
-import com.example.forsubmit.domain.auth.properties.OAuthBaseProperty
-import com.example.forsubmit.domain.auth.properties.OAuthPKCE
-import com.example.forsubmit.domain.auth.properties.OAuthProperties
+import com.example.forsubmit.domain.auth.oauthparams.GithubOAuthParams
+import com.example.forsubmit.domain.auth.oauthparams.GoogleOAuthParams
+import com.example.forsubmit.domain.auth.payload.response.OAuthRedirectUriResponse
+import com.example.forsubmit.domain.auth.properties.GithubOAuthProperties
+import com.example.forsubmit.domain.auth.properties.GoogleOAuthProperties
 import com.example.forsubmit.global.payload.BaseResponse
 import org.springframework.stereotype.Service
-import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
 import org.springframework.web.util.UriComponentsBuilder
 
 
 @Service
 class OAuthService(
-    private val oAuthProperties: OAuthProperties
+    private val githubOAuthProperties: GithubOAuthProperties,
+    private val googleOAuthProperties: GoogleOAuthProperties
 ) {
 
     companion object {
@@ -22,11 +23,21 @@ class OAuthService(
         const val GET_AUTH_URL_MESSAGE_KOR = "인증 URL을 성공적으로 반환했습니다."
     }
 
-    fun getAuthenticationUri(type: String): BaseResponse<OAuthRedirectUrlResponse> {
+    fun getAuthorizeUri(
+        type: String,
+        codeChallenge: String?,
+        codeChallengeMethod: String?
+    ): BaseResponse<OAuthRedirectUriResponse> {
 
-        val redirectUri = when (type) {
-            "google" -> getAuthUri(oAuthProperties.google)
-            "github" -> getAuthUri(oAuthProperties.github)
+        val authUri = when (type) {
+            "github" -> {
+                val params = GithubOAuthParams(githubOAuthProperties)
+                getAuthUri(params.getParams(), GithubOAuthProperties.BASE_URL, GithubOAuthProperties.AUTHORIZE_URL)
+            }
+            "google" -> {
+                val params = GoogleOAuthParams(codeChallenge!!, codeChallengeMethod!!, googleOAuthProperties)
+                getAuthUri(params.getParams(), GoogleOAuthProperties.BASE_URL, GoogleOAuthProperties.AUTHORIZE_URL)
+            }
             else -> throw InvalidOauthTypeException.EXCEPTION
         }
 
@@ -34,43 +45,16 @@ class OAuthService(
             status = 200,
             message = GET_AUTH_URL_MESSAGE,
             koreanMessage = GET_AUTH_URL_MESSAGE_KOR,
-            content = OAuthRedirectUrlResponse(redirectUri)
+            content = OAuthRedirectUriResponse(authUri)
         )
     }
 
-    private fun getAuthUri(baseProperty: OAuthBaseProperty): String {
-        val params = buildParams(baseProperty)
-
+    private fun getAuthUri(params: MultiValueMap<String, String>, baseUrl: String, authorizeUri: String): String {
         return UriComponentsBuilder
-            .fromHttpUrl(baseProperty.baseUrl)
-            .path(baseProperty.authorizeUri)
+            .fromHttpUrl(baseUrl)
+            .path(authorizeUri)
             .queryParams(params)
             .toUriString()
-    }
-
-    private fun buildParams(properties: OAuthBaseProperty): MultiValueMap<String, String> {
-        val params = when (properties) {
-            is OAuthPKCE -> buildChallengeParam(properties)
-            else -> LinkedMultiValueMap()
-        }
-
-        params["client_id"] = properties.clientId
-        params["redirect_uri"] = properties.redirectUrl
-        params["scope"] = properties.scope
-        params["response_type"] = "code"
-
-        return params
-    }
-
-    private fun buildChallengeParam(oAuthPKCE: OAuthPKCE): MultiValueMap<String, String> {
-        val params = LinkedMultiValueMap<String, String>()
-        val codeVerifier = oAuthPKCE.generateCodeVerifier()
-        val codeChallenge = oAuthPKCE.generateCodeChallenge(codeVerifier)
-
-        params["code_challenge_method"] = "S256"
-        params["code_challenge"] = codeChallenge
-
-        return params
     }
 
 }
