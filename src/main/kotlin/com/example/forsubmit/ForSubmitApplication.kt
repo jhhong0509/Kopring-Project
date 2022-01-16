483cf875550c9f3ff9f9a9ae3af6888b3e2ee1c3
jhhong0509 <jhhong0509@naver.com>
2022-01-16 23:12:02 +0900

    ⚡️ :: (#125) @EnableFeignClients 추가

--- src/main/kotlin/com/example/forsubmit/ForSubmitApplication.kt
+++ src/main/kotlin/com/example/forsubmit/ForSubmitApplication.kt
@@ -3,11 +3,13 @@ package com.example.forsubmit
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 const val BASE_PACKAGE = "com.example.forsubmit"
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableFeignClients
 class ForSubmitApplication
 
 fun main(args: Array<String>) {
