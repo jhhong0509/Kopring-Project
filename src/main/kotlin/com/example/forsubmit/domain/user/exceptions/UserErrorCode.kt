af4f98325ac5a8559c58953432b50ba48437b0fb
jhhong0509 <jhhong0509@naver.com>
2022-01-17 00:21:38 +0900

    ⚡️ :: (#125) InvalidOAuthTypeException

--- src/main/kotlin/com/example/forsubmit/domain/user/exceptions/UserErrorCode.kt
+++ src/main/kotlin/com/example/forsubmit/domain/user/exceptions/UserErrorCode.kt
@@ -8,6 +8,5 @@ enum class UserErrorCode(
     override val koreanMessage: String
 ): ExceptionProperty {
     USER_NOT_FOUND(404, "User Not Found", "사용자를 찾을 수 없습니다."),
-    EMAIL_ALREADY_EXISTS(409, "Email Already Exists", "이 이메일이 이미 사용중입니다."),
-    INVALID_OAUTH_TYPE(400, "Invalid OAuth Token", "지원되지 않는 OAuth 종류입니다.")
+    EMAIL_ALREADY_EXISTS(409, "Email Already Exists", "이 이메일이 이미 사용중입니다.")
 }
