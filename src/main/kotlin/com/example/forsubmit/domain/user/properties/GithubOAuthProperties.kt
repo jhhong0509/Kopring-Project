d9e0b5f31d9e30d19644fb15da2734504e76501c
jhhong0509 <jhhong0509@naver.com>
2022-01-17 23:08:42 +0900

    ⚡️ :: (#125) Properties 수정

--- src/main/kotlin/com/example/forsubmit/domain/user/properties/GithubOAuthProperties.kt
+++ src/main/kotlin/com/example/forsubmit/domain/user/properties/GithubOAuthProperties.kt
@@ -1,4 +1,4 @@
-package com.example.forsubmit.domain.auth.properties
+package com.example.forsubmit.domain.user.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
@@ -7,13 +7,22 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 @ConstructorBinding
 @ConfigurationProperties(prefix = "oauth2.github")
-class GithubOAuthProperties : OAuthBaseProperty {
+class GithubOAuthProperties : BaseOAuthProperty {
     override lateinit var clientId: String
     override lateinit var clientSecret: String
     override lateinit var redirectUri: String
     override lateinit var scope: String
+
     companion object {
-        const val BASE_URL = "https://github.com/login/oauth"
-        const val AUTHORIZE_URL = "/authorize"
+        const val AUTHORIZE_BASE_URL: String = "https://github.com/login/oauth"
+        const val AUTHORIZE_ENDPOINT: String = "/authorize"
+
+        const val TOKEN_BASE_URL: String = "https://github.com/login/oauth"
+        const val TOKEN_ENDPOINT: String = "/access_token"
+
+        const val USER_INFO_BASE_URL: String = "https://api.github.com"
+        const val USER_INFO_ENDPOINT: String = "/user"
+
+        const val ACCEPT: String = "application/vnd.github.v3+json"
     }
 }
--- src/main/kotlin/com/example/forsubmit/domain/user/properties/GoogleOAuthProperties.kt
+++ src/main/kotlin/com/example/forsubmit/domain/user/properties/GoogleOAuthProperties.kt
@@ -1,4 +1,4 @@
-package com.example.forsubmit.domain.auth.properties
+package com.example.forsubmit.domain.user.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
@@ -7,17 +7,22 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 @ConstructorBinding
 @ConfigurationProperties(prefix = "oauth2.google")
-class GoogleOAuthProperties : OAuthBaseProperty {
+class GoogleOAuthProperties : BaseOAuthProperty {
     override lateinit var clientId: String
     override lateinit var clientSecret: String
     override lateinit var redirectUri: String
     override lateinit var scope: String
 
     companion object {
-        const val BASE_URL = "https://accounts.google.com/o/oauth2/v2"
-        const val OAUTH_BASE_URL = "https://oauth2.googleapis.com"
-        const val AUTHORIZE_URL = "/auth"
-        const val TOKEN_URL = "/token"
-        const val USER_INFO_URL = "/tokeninfo"
+        const val AUTHORIZE_BASE_URL: String = "https://accounts.google.com/o/oauth2/v2"
+        const val AUTHORIZE_ENDPOINT: String = "/auth"
+
+        const val TOKEN_BASE_URL: String = "https://oauth2.googleapis.com"
+        const val TOKEN_ENDPOINT: String = "/token"
+
+        const val USER_INFO_BASE_URL: String = "https://www.googleapis.com/oauth2/v2"
+        const val USER_INFO_ENDPOINT: String = "/userinfo"
+
+        const val ACCEPT: String = "*/*"
     }
 }
