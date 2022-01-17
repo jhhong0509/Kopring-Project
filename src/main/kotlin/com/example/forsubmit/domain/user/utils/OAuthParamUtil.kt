31ff3279d454b69362cddc1b627d0057f91914fd
jhhong0509 <jhhong0509@naver.com>
2022-01-17 23:26:26 +0900

    ⚡️ :: (#125) 이름 변경

--- src/main/kotlin/com/example/forsubmit/domain/user/utils/OAuthParamUtil.kt
+++ src/main/kotlin/com/example/forsubmit/domain/user/utils/OAuthParamUtil.kt
@@ -1,42 +1,42 @@
 package com.example.forsubmit.domain.user.utils
 
-import com.example.forsubmit.domain.user.oauthparams.authorize.OAuthBaseAuthorizeParam
-import com.example.forsubmit.domain.user.oauthparams.authorize.OAuthPKCEAuthorizeParam
-import com.example.forsubmit.domain.user.oauthparams.token.OAuthBaseTokenParam
-import com.example.forsubmit.domain.user.oauthparams.token.OAuthPKCETokenParam
+import com.example.forsubmit.domain.user.dtos.authorize.OAuthBaseAuthorizeDto
+import com.example.forsubmit.domain.user.dtos.authorize.OAuthPKCEAuthorizeDto
+import com.example.forsubmit.domain.user.dtos.token.OAuthBaseTokenDto
+import com.example.forsubmit.domain.user.dtos.token.OAuthPKCETokenDto
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
 
 class OAuthParamUtil {
     companion object {
-        fun buildAuthorizeParam(oAuthBaseAuthorizeParam: OAuthBaseAuthorizeParam): MultiValueMap<String, String> {
+        fun buildAuthorizeParam(oAuthBaseAuthorizeDto: OAuthBaseAuthorizeDto): MultiValueMap<String, String> {
             val params = LinkedMultiValueMap<String, String>()
 
-            if (oAuthBaseAuthorizeParam is OAuthPKCEAuthorizeParam) {
-                params["code_challenge"] = oAuthBaseAuthorizeParam.codeChallenge
-                params["code_challenge_method"] = oAuthBaseAuthorizeParam.codeChallengeMethod
+            if (oAuthBaseAuthorizeDto is OAuthPKCEAuthorizeDto) {
+                params["code_challenge"] = oAuthBaseAuthorizeDto.codeChallenge
+                params["code_challenge_method"] = oAuthBaseAuthorizeDto.codeChallengeMethod
             }
 
             params["response_type"] = "code"
-            params["client_id"] = oAuthBaseAuthorizeParam.baseOAuthProperty.clientId
-            params["scope"] = oAuthBaseAuthorizeParam.baseOAuthProperty.scope
-            params["redirect_uri"] = oAuthBaseAuthorizeParam.baseOAuthProperty.redirectUri
+            params["client_id"] = oAuthBaseAuthorizeDto.baseOAuthProperty.clientId
+            params["scope"] = oAuthBaseAuthorizeDto.baseOAuthProperty.scope
+            params["redirect_uri"] = oAuthBaseAuthorizeDto.baseOAuthProperty.redirectUri
 
             return params
         }
 
-        fun buildBaseTokenParam(oAuthBaseTokenParam: OAuthBaseTokenParam): MultiValueMap<String, String> {
+        fun buildBaseTokenParam(oAuthBaseTokenDto: OAuthBaseTokenDto): MultiValueMap<String, String> {
             val params = LinkedMultiValueMap<String, String>()
 
-            if (oAuthBaseTokenParam is OAuthPKCETokenParam) {
-                params["code_verifier"] = oAuthBaseTokenParam.codeVerifier
+            if (oAuthBaseTokenDto is OAuthPKCETokenDto) {
+                params["code_verifier"] = oAuthBaseTokenDto.codeVerifier
             }
 
             params["grant_type"] = "authorization_code"
-            params["code"] = oAuthBaseTokenParam.code
-            params["redirect_uri"] = oAuthBaseTokenParam.baseOAuthProperty.redirectUri
-            params["client_id"] = oAuthBaseTokenParam.baseOAuthProperty.clientId
-            params["client_secret"] = oAuthBaseTokenParam.baseOAuthProperty.clientSecret
+            params["code"] = oAuthBaseTokenDto.code
+            params["redirect_uri"] = oAuthBaseTokenDto.baseOAuthProperty.redirectUri
+            params["client_id"] = oAuthBaseTokenDto.baseOAuthProperty.clientId
+            params["client_secret"] = oAuthBaseTokenDto.baseOAuthProperty.clientSecret
 
             return params
         }
