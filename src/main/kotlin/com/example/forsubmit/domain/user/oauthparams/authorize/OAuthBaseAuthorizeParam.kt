cfc0bb41de71845a4df310d1754696a9f9875e99
jhhong0509 <jhhong0509@naver.com>
2022-01-17 23:23:43 +0900

    ⚡️ :: (#125) BaseAuthorizeDto 추가

--- src/main/kotlin/com/example/forsubmit/domain/user/oauthparams/authorize/OAuthBaseAuthorizeParam.kt
+++
@@ -1,7 +0,0 @@
-package com.example.forsubmit.domain.auth.oauthparams.authorize
-
-import com.example.forsubmit.domain.auth.properties.OAuthBaseProperty
-
-interface OAuthBaseAuthorizeParam {
-    val oAuthBaseProperty: OAuthBaseProperty
-}
