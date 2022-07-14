b4639e3c2c766912446bb2950467f8467510ae6f
jhhong0509 <jhhong0509@naver.com>
2022-07-14 18:50:14 +0900

    feat: delete unused annotations

--- src/main/kotlin/com/example/forsubmit/global/socket/annotation/SocketController.kt
+++
@@ -1,8 +0,0 @@
-package com.example.forsubmit.global.socket.annotation
-
-import org.springframework.web.bind.annotation.RestController
-
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-@RestController
-annotation class SocketController
